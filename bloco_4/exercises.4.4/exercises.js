2d61c8578d8753fb3e9912469fd47af5c90efed0
Ana Ventura <anaventura1811@gmail.com>
2021-03-01 15:59:25 -0300

    Imprime no console mensagem de boas-vindas para a personagem

---
+++ bloco_4/exercises.4.4/exercises.js
@@ -0,0 +1,9 @@
+//Exercícios - Dia 4.4 - Parte I - Objetos e For/In//
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+  console.log('Bem-vinda, ' + info.personagem);
